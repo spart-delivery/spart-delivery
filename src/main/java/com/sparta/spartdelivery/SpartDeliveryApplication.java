7cfe908a735096635828a2c5c40052c7c0d78914
oht <duduio2050@gmail.com>
2024-09-19 13:16:40 +0900

    init

---
+++ src/main/java/com/sparta/spartdelivery/SpartDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spartdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartDeliveryApplication.class, args);
+    }
+
+}
