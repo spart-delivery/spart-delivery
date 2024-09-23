7146147e75327d2cf7894b34cf74667f5a6475bf
Xabier <94465466+XabierJEONG@users.noreply.github.com>
2024-09-23 16:08:11 +0900

    feat - reviewServiceTest 작성(리뷰저장 및 조회)

--- src/main/java/com/sparta/spartdelivery/domain/review/dto/requestDto/ReviewEditRequestDto.java
+++ src/main/java/com/sparta/spartdelivery/domain/review/dto/requestDto/ReviewEditRequestDto.java
@@ -1,8 +1,12 @@
 package com.sparta.spartdelivery.domain.review.dto.requestDto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReviewEditRequestDto {
 
     private Integer starPoint;
